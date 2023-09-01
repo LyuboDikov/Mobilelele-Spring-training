742bc91458b9d17b55b597d3c7357680488af388
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-01 20:05:49 +0300

    login html updated, User login controller created

---
+++ src/main/java/bg/softuni/Mobilelele/web/UserLoginController.java
@@ -0,0 +1,21 @@
+package bg.softuni.Mobilelele.web;
+
+import bg.softuni.Mobilelele.models.dtos.UserLoginDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserLoginController {
+
+    @GetMapping("/users/login")
+    public String login() {
+        return "auth-login";
+    }
+
+    @PostMapping("/user/login")
+    public String login(UserLoginDto userLoginDto) {
+        System.out.println(userLoginDto);
+        return "redirect:/";
+    }
+}
