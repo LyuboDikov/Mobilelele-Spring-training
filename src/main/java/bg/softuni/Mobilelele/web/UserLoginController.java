932313703303bbecb23fbd6237088c29cb1547fc
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-01 20:17:06 +0300

    Small changes

--- src/main/java/bg/softuni/Mobilelele/web/UserLoginController.java
+++ src/main/java/bg/softuni/Mobilelele/web/UserLoginController.java
@@ -1,12 +1,19 @@
 package bg.softuni.Mobilelele.web;
 
 import bg.softuni.Mobilelele.models.dtos.UserLoginDto;
+import bg.softuni.Mobilelele.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserLoginController {
+
+    private final UserService userService;
+
+    public UserLoginController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users/login")
     public String login() {
@@ -15,7 +22,7 @@ public class UserLoginController {
 
     @PostMapping("/user/login")
     public String login(UserLoginDto userLoginDto) {
-        System.out.println(userLoginDto);
+        System.out.println("User is logged: " + userService.login(userLoginDto));
         return "redirect:/";
     }
 }
