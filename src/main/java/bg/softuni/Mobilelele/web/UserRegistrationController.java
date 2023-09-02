a251b01ed9c328c423a04e012858141332f1b1b9
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-02 12:42:43 +0300

    Fixed auth-register html file. Created UserRegisterDto and UserRegistration controller

---
+++ src/main/java/bg/softuni/Mobilelele/web/UserRegistrationController.java
@@ -0,0 +1,24 @@
+package bg.softuni.Mobilelele.web;
+
+import bg.softuni.Mobilelele.models.dtos.UserRegisterDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserRegistrationController {
+
+    @GetMapping("/users/register")
+    public String register(){
+        return "auth-register";
+    }
+
+
+    @PostMapping
+    public String register(UserRegisterDto userRegisterDto) {
+
+        return "redirect:/";
+    }
+
+//TODO Explain POST-redirect-GET
+}
