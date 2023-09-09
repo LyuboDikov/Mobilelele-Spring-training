77b21e7969a778cc4d70db465f2e23076df772e7
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-09 12:39:49 +0300

    Fixed initUserModel method

--- src/main/java/bg/softuni/Mobilelele/web/UserRegistrationController.java
+++ src/main/java/bg/softuni/Mobilelele/web/UserRegistrationController.java
@@ -28,8 +28,8 @@ public class UserRegistrationController {
     // We can add another model attribute with different name
     // and different object, so we can use it too
     @ModelAttribute("userModel")
-    public void initUserModel(Model model) {
-        model.addAttribute("userModel", new UserRegisterDto());
+    public UserRegisterDto initUserModel() {
+        return new UserRegisterDto();
     }
 
     @GetMapping("/register")
