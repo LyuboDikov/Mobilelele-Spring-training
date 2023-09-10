6cdde51152708e59939311c3a56f3a839246e254
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-10 15:35:58 +0300

    Created OfferService and impl

---
+++ src/main/java/bg/softuni/Mobilelele/services/OfferService.java
@@ -0,0 +1,4 @@
+package bg.softuni.Mobilelele.services;
+
+public interface OfferService {
+}
---
+++ src/main/java/bg/softuni/Mobilelele/services/impl/OfferServiceImpl.java
@@ -0,0 +1,8 @@
+package bg.softuni.Mobilelele.services.impl;
+
+import bg.softuni.Mobilelele.services.OfferService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferServiceImpl implements OfferService {
+}
