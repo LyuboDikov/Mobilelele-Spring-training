81051eb778b2a23f01df54c28e0e6879bc1ed973
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-11 14:59:01 +0300

    Fixed UserController to get the binding result errors and to initialize userLoginDto in the model attributes

--- src/main/java/bg/softuni/Mobilelele/web/UserController.java
+++ src/main/java/bg/softuni/Mobilelele/web/UserController.java
@@ -2,10 +2,14 @@ package bg.softuni.Mobilelele.web;
 
 import bg.softuni.Mobilelele.models.dtos.UserLoginDto;
 import bg.softuni.Mobilelele.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -15,6 +19,11 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @ModelAttribute("userModel")
+    public UserLoginDto initUserModel() {
+        return new UserLoginDto();
     }
 
     @GetMapping("/login")
@@ -29,7 +38,21 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(UserLoginDto userLoginDto) {
+    public String login(@Valid UserLoginDto userLoginDto,
+                        BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes) {
+
+
+        if(bindingResult.hasErrors() || !userService.login(userLoginDto)) {
+            redirectAttributes.addFlashAttribute("userModel", userLoginDto);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel",
+                    bindingResult);
+
+            bindingResult.rejectValue("password", "InvalidPasswordError", "Invalid password.");
+
+            return "redirect:/users/login";
+        }
+
         userService.login(userLoginDto);
         return "redirect:/";
     }
